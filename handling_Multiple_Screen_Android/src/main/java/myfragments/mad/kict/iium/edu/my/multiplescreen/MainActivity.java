16fd15a45a5d82d9989614334ffbb929acccd491
Jawid Arabzada <36728511+Jawid-Arabzada@users.noreply.github.com>
2018-04-04 01:28:56 +0800

    Add files via upload

---
+++ handling_Multiple_Screen_Android/src/main/java/myfragments/mad/kict/iium/edu/my/multiplescreen/MainActivity.java
@@ -0,0 +1,17 @@
+package myfragments.mad.kict.iium.edu.my.multiplescreen;
+
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
